e0a824e4f35fd3e4e207b9814dec6b010f3cf75d
Sedef Cakmak <cakmaksedef@gmail.com>
2022-10-28 21:39:14 +0300

    implemented onClick for the thumbnail image and the display image

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -14,15 +14,27 @@ import {
 import { ReactComponent as Previous } from "../images/icon-previous.svg";
 import { ReactComponent as Next } from "../images/icon-next.svg";
 
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 const Product = () => {
   const [isLargerThanMD] = useMediaQuery("(min-width: 48em)");
+  const [displayPicture, setDisplayPicture] = useState(
+    "/images/image-product-1.jpg"
+  );
+  //const [isActive, setIsActive] = useState(null);
+
+  function changeDisplayPicture(e) {
+    let displaySource = e.currentTarget.getAttribute("src_display");
+    setDisplayPicture(displaySource);
+    // setIsActive(true);
+  }
   return (
     <>
       <Box ms="30px">
         <Box
           boxSize="xs"
           position={"relative"}
-          border="1px solid red"
           // width="100%"
           // maxWidth={"375px"}
         >
@@ -38,14 +50,24 @@ const Product = () => {
               icon={<Previous />}
             />
           )}
+          {/* <motion.div
+            className="container"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              type: "tween",
+              duration: 5,
+              ease: "easeOut",
+            }}
+          > */}
           <Image
-            src={"/images/image-product-1.jpg"}
+            src={displayPicture}
             alt="Product first image"
             rounded="xl"
-            // objectFit="cover"
-            // w="100%"
             _hover={{ cursor: "pointer" }}
           />
+          {/* </motion.div> */}
+
           {!isLargerThanMD && (
             <IconButton
               variant="ghost"
@@ -62,7 +84,7 @@ const Product = () => {
         </Box>
         {isLargerThanMD && (
           <Box
-            maxW="384px"
+            maxW="320px"
             my="30px"
           >
             <Flex justify={"space-between"}>
@@ -70,10 +92,22 @@ const Product = () => {
                 <Image
                   key={image.name}
                   src={image.src_thumbnail}
+                  src_display={image.src_display}
                   alt={image.name_thumbnail}
+                  // className={
+                  //   image.src_display === displayPicture ? "active" : ""
+                  // }
+                  display="block"
+                  verticalAlign="bottom"
+                  border={
+                    image.src_display === displayPicture
+                      ? "2px solid orange"
+                      : "2px solid transparent"
+                  }
                   rounded="xl"
                   w="62px"
-                  _hover={{ cursor: "pointer" }}
+                  _hover={{ cursor: "pointer", opacity: "0.5" }}
+                  onClick={(e) => changeDisplayPicture(e)}
                 />
               ))}
             </Flex>
@@ -87,25 +121,25 @@ const Product = () => {
 const ProductImages = [
   {
     name: "Product image 1",
-    src: "/images/image-product-1.jpg",
+    src_display: "/images/image-product-1.jpg",
     name_thumbnail: "Product Thumbnail image 1",
     src_thumbnail: "/images/image-product-1-thumbnail.jpg",
   },
   {
     name: "Product image 2",
-    src: "/images/image-product-2.jpg",
+    src_display: "/images/image-product-2.jpg",
     name_thumbnail: "Product Thumbnail image 2",
     src_thumbnail: "/images/image-product-2-thumbnail.jpg",
   },
   {
     name: "Product image 3",
-    src: "/images/image-product-3.jpg",
+    src_display: "/images/image-product-3.jpg",
     name_thumbnail: "Product Thumbnail image 3",
     src_thumbnail: "/images/image-product-3-thumbnail.jpg",
   },
   {
     name: "Product image 4",
-    src: "/images/image-product-4.jpg",
+    src_display: "/images/image-product-4.jpg",
     name_thumbnail: "Product Thumbnail image 4",
     src_thumbnail: "/images/image-product-4-thumbnail.jpg",
   },
